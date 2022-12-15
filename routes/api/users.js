6e7627e2eea6ef092ca5599831b1649f88280e45
Ben Gunn <bengunn.dev@gmail.com>
2022-12-16 05:26:56 +0630

    add user roles

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,15 @@
 import express from "express";
 import * as userController from "../../controllers/userController.js";
+import ROLES_LIST from "../../config/roles_list.js";
+import verifyRoles from "../../middleware/verifyRoles.js";
 const router = express.Router();
 
 
-router.get('/', userController.getAllUsers);
+router.route('/')
+    .get(verifyRoles(ROLES_LIST.Admin), userController.getAllUsers)
+    .delete(verifyRoles(ROLES_LIST.Admin), userController.deleteUser);
+
+router.route('/:id')
+    .get(verifyRoles(ROLES_LIST.Admin), userController.getUser);
 
 export default router;
